af4148c0c08dfa43826083033511d2760236f404
jKakZyo <vlasssov17@gmail.com>
2024-12-21 14:22:26 +0500

    Update errors.go

--- pack /errors.go
+++ pack /errors.go
@@ -1,1 +1,11 @@
+package calculator
 
+import "errors"
+
+var (
+	ErrMismatchedParentheses = errors.New("mismatched parentheses")
+	ErrInvalidExpression      = errors.New("invalid expression")
+	ErrDivisionByZero        = errors.New("division by zero")
+	ErrInvalidNumber          = errors.New("invalid number")
+	ErrInternalServerError    = errors.New("internal server error")
+)
